684e5597c569fa26796544285286dd09ceb5c7ce
aylaputri <aylaputri12@upi.edu>
2025-05-29 18:20:48 +0700

    Ayla | logic nembak lagi

--- src/utils/player.cpp
+++ src/utils/player.cpp
@@ -40,6 +40,33 @@ void shootPlayerUp(vector<vector<Vertex *>> &map, pair<int, int> position, bool 
     }
 }
 
+void shootPlayerDown(vector<vector<Vertex *>> &map, pair<int, int> &position, bool first=true)
+{
+    Vertex *v = map[position.first][position.second];
+    if (!first) v->shoot = true;
+    if (v->up && v->weightDown > 0){
+         shootPlayerUp(map, {v->up->x, v->up->y}, false);
+    }   
+}
+
+void shootPlayerLeft(vector<vector<Vertex *>> &map, pair<int, int> &position, bool first=true)
+{
+    Vertex *v = map[position.first][position.second];
+    if (!first) v->shoot = true;
+    if (v->up && v->weightLeft > 0){
+         shootPlayerUp(map, {v->up->x, v->up->y}, false);
+    }   
+}
+
+void shootPlayerRight(vector<vector<Vertex *>> &map, pair<int, int> &position, bool first=true)
+{
+    Vertex *v = map[position.first][position.second];
+    if (!first) v->shoot = true;
+    if (v->up && v->weightRight > 0){
+         shootPlayerUp(map, {v->up->x, v->up->y}, false);
+    }   
+}
+
 void controlPlayer(vector<vector<Vertex *>> &map, char input)
 {
     switch (input)
@@ -58,6 +85,15 @@ void controlPlayer(vector<vector<Vertex *>> &map, char input)
         break; 
     case 'w':
         shootPlayerUp(map,currentPos);
+        break;
+    case 's':
+        shootPlayerDown(map, currentPos);
+        break;
+    case 'a':
+        shootPlayerLeft(map, currentPos);
+        break;
+    case 'd':
+        shootPlayerRight(map, currentPos);
         break;
     }
    
