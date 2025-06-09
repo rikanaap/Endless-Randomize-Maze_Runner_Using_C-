96032f265c1c606a999767332fc9137d5ccb65bb
aylaputri <aylaputri12@upi.edu>
2025-06-09 15:19:59 +0700

    Ayla| add design kalah

---
+++ src/design/kalah.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void tampilanKalah() {
+    cout << "\n";
+    cout << "╔═══════════════════════╗\n";
+    cout << "║      GAME OVER ☠️     ║\n";
+    cout << "╚═══════════════════════╝\n";
+    cout << "\n";
+}
+
+int main() {
+    tampilanKalah();
+}
